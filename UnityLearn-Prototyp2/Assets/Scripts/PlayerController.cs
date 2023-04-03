3fef3956476a0ae9098b5307453e59f81d08e185
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2023-04-03 21:16:00 +0300

    Fixing "keep player inbound"

--- UnityLearn-Prototyp2/Assets/Scripts/PlayerController.cs
+++ UnityLearn-Prototyp2/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
-    public float speed = 0.5f;
+    public float speed = 10f;
+    public float xRange = 15;
 
     // Start is called before the first frame update
     void Start()
@@ -16,18 +17,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //keep the player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
         //move left and right
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        //keep the player in bounds
-        if (transform.position.x < 10)
-        {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
-        }
-        if (transform.position.x > 10)
-        {
-            transform.position = new Vector3(10, transform.position.y, transform.position.z);
-        }
+        
+        
+        
     }
 }
