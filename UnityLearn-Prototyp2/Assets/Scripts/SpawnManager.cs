9de4b7abecc9a92d7e08eff210c3f53a487f37f0
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2023-04-25 14:35:06 +0300

    Spawn the animals at timed intervals

--- UnityLearn-Prototyp2/Assets/Scripts/SpawnManager.cs
+++ UnityLearn-Prototyp2/Assets/Scripts/SpawnManager.cs
@@ -8,24 +8,28 @@ public class SpawnManager : MonoBehaviour
 
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            //Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+    
+    }
 
-            //Spawn random animals from array
-            int animalIndex = Random.Range(0, animaPrefabs.Length);
-            Instantiate(animaPrefabs[animalIndex], spawnPos, animaPrefabs[animalIndex].transform.rotation);
-        }
+    void SpawnRandomAnimal()
+    {
+        //Randomly generate animal index and spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        //Spawn random animals from array
+        int animalIndex = Random.Range(0, animaPrefabs.Length);
+        Instantiate(animaPrefabs[animalIndex], spawnPos, animaPrefabs[animalIndex].transform.rotation);
     }
 }
