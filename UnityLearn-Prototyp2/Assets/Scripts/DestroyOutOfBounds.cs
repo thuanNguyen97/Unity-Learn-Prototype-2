ba0b83b97d10a562eaab703d4f81a6c64bcb6b89
ThuanNguyenLAB <nguyencuuthuan2001@gmail.com>
2023-04-26 13:29:02 +0300

    Debug Game over

--- UnityLearn-Prototyp2/Assets/Scripts/DestroyOutOfBounds.cs
+++ UnityLearn-Prototyp2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -22,6 +22,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }    
         else if (transform.position.z < _lowerBound)
         {
+            Debug.Log("Game Over");
             Destroy(gameObject);
         }    
     }
